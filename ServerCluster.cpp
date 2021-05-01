e1d8f285abd48e43b3b6b4cdf1fe724588b406c9
Spivak Alexander <po4ta.polevaya@gmail.com>
2021-05-01 14:33:25 +0300

    Главный цикл run разбит на методы.

--- ServerCluster.cpp
+++ ServerCluster.cpp
@@ -12,93 +12,74 @@ ServerCluster &ServerCluster::Instance() {
 void ServerCluster::setup() {
   //TODO: цикл со всеми серверами
   Server server(IP, PORT);
-  _servers.insert(std::make_pair(server.getLS(), server));
+  server.run();
+  _servers.insert(std::make_pair(server.get_socket(), server));
   Server server1(IP2, PORT);
-  _servers.insert(std::make_pair(server1.getLS(), server1));
+  server1.run();
+  _servers.insert(std::make_pair(server1.get_socket(), server1));
+}
+
+int ServerCluster::_set_fds(fd_set *readfds, fd_set* writefds) {
+  int max_fd = _servers.begin()->first;
+  FD_ZERO(readfds);
+  FD_ZERO(writefds);
+  std::map<int, Server>::iterator begin = _servers.begin();
+  std::map<int, Server>::iterator end = _servers.end();
+  while (begin != end) {
+    Server &server = begin->second;
+    max_fd = std::max(max_fd, server.set_fds(readfds, writefds));
+    ++begin;
+  }
+  return max_fd;
+}
+
+void ServerCluster::_accept_connection(fd_set *readfds) {
+  std::map<int, Server>::iterator begin = _servers.begin();
+  std::map<int, Server>::iterator end = _servers.end();
+  while (begin != end) {
+    Server &server = begin->second;
+    if (FD_ISSET(server.get_socket(), readfds)) {
+      server.accept();
+    }
+    ++begin;
+  }
+}
+
+void ServerCluster::_read_request(fd_set *readfds) {
+  std::map<int, Server>::iterator begin = _servers.begin();
+  std::map<int, Server>::iterator end = _servers.end();
+  while (begin != end) {
+    Server &server = begin->second;
+    server.read_request(readfds);
+    ++begin;
+  }
+}
+
+void ServerCluster::_send_response(fd_set *writefds) {
+  std::map<int, Server>::iterator begin = _servers.begin();
+  std::map<int, Server>::iterator end = _servers.end();
+  while (begin != end) {
+    Server &server = begin->second;
+    server.send_response(writefds);
+    ++begin;
+  }
 }
 
 void ServerCluster::run() {
   //TODO: Реализовать функционал.
   while (1) {
     fd_set readfds, writefds;
-    int max_d = _servers.rbegin()->first;
-    FD_ZERO(&readfds);
-    FD_ZERO(&writefds);
-    std::map<int, Server>::iterator begin = _servers.begin();
-    std::map<int, Server>::iterator end = _servers.end();
-    while (begin != end) {
-      FD_SET(begin->first, &readfds);
-      max_d = max_d < begin->first ? begin->first : max_d;
-      std::map<int, Session>::iterator cli_begin = begin->second.sessions.begin();
-      std::map<int, Session>::iterator cli_end = begin->second.sessions.end();
-      while (cli_begin != cli_end) {
-        FD_SET(cli_begin->first, &readfds);
-        if (cli_begin->second.state == fsm_write) {
-          FD_SET(cli_begin->first, &writefds);
-        }
-        max_d = max_d < cli_begin->first ? cli_begin->first : max_d;
-        cli_begin++;
-      }
-      ++begin;
-    }
-    int res = select(max_d + 1, &readfds, &writefds, 0, 0);
+    int max_fd = _set_fds(&readfds, &writefds);
+    int res = select(max_fd + 1, &readfds, &writefds, 0, 0);
     if (res == -1) {
       std::cerr << "Select error" << std::endl; //TODO: ???
       continue;
     } else if (res == 0) {
       continue; // тайм аут
     }
-
-    {
-      std::map<int, Server>::iterator begin = _servers.begin();
-      std::map<int, Server>::iterator end = _servers.end();
-      while (begin != end) {
-        if (FD_ISSET(begin->first, &readfds)) {
-          begin->second.accept();
-        }
-        ++begin;
-      }
-    }
-
-    {
-      std::map<int, Server>::iterator begin = _servers.begin();
-      std::map<int, Server>::iterator end = _servers.end();
-      while (begin != end) {
-        std::map<int, Session>::iterator cli_begin = begin->second.sessions.begin();
-        std::map<int, Session>::iterator cli_end = begin->second.sessions.end();
-        while (cli_begin != cli_end) {
-          if (FD_ISSET(cli_begin->first, &readfds)) {
-            read(cli_begin->first, cli_begin->second.buf, BUF_SIZE);
-            cli_begin->second.state = fsm_write;
-            std::cout << cli_begin->second.buf << std::endl;
-          }
-          ++cli_begin;
-        }
-        ++begin;
-      }
-    }
-
-    {
-      std::map<int, Server>::iterator begin = _servers.begin();
-      std::map<int, Server>::iterator end = _servers.end();
-      while (begin != end) {
-        std::map<int, Session>::iterator cli_begin = begin->second.sessions.begin();
-        std::map<int, Session>::iterator cli_end = begin->second.sessions.end();
-        while (cli_begin != cli_end) {
-          if (FD_ISSET(cli_begin->first, &writefds)) {
-            std::string answer = "HTTP/1.1 200 OK\n\n<h1>Hello</h1>\n";
-            write(cli_begin->first, answer.c_str(), answer.length());
-            int tmp = cli_begin->first;
-            ++cli_begin;
-            begin->second.closeSession(tmp);
-          } else {
-            ++cli_begin;
-          }
-        }
-        ++begin;
-      }
-    }
-    ++begin;
+    this->_accept_connection(&readfds);
+    this->_read_request(&readfds);
+    this->_send_response(&writefds);
   }
 }
 
--- ServerCluster.hpp
+++ ServerCluster.hpp
@@ -26,9 +26,13 @@ class ServerCluster {
  private:
   std::map<int, Server> _servers;
 
+  int _set_fds(fd_set *readfds, fd_set *writefds);
   ServerCluster();
   ServerCluster(ServerCluster const &);
   ServerCluster& operator=(ServerCluster const &);
+  void _accept_connection(fd_set *readfds);
+  void _read_request(fd_set *readfds);
+  void _send_response(fd_set *writefds);
 };
 
 #endif //WEBSERV__SERVERCLUSTER_HPP_
