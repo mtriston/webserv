72d0fa5e3ff2f84bbb9bc683c58c114414f6dd40
ksilver-bikey <71587933+ksilver-bikey@users.noreply.github.com>
2021-06-06 18:39:46 +0300

    Update Config_unit.cpp

--- Config_unit.cpp
+++ Config_unit.cpp
@@ -340,6 +340,20 @@ bool config_unit::_pathComp(char const *path, char const *iter)
 
 std::string config_unit::getUploadPath(std::string const &path)
 {
+	int				cnt;
+	std::string    	res;
+	std::map<std::string, location_unit>::iterator it = _getLocation(path);
 	
-	return _getLocation(path)->second._storage;
+	if (_location.empty())
+		return (std::string());
+	res = it->second._storage;
+	cnt = it->first.size();
+	if (path[0] == '/' && res[res.size() - 1] == '/')
+		++cnt;
+	if (res[res.size() - 1] != '/')
+		res.append("/");
+	res.append(&path[cnt]);
+	if (res[res.size() - 1] == '/')
+		res.append(it->second._def_file);
+	return res;
 }
